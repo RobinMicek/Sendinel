c160908623f88f432c7921bd0d00ea8777b351fb
Robin Míček <micek.robin@gmail.com>
2025-09-02 20:21:27 +0200

    fix(frontend): fixed icon stroke color for datatable badges

--- frontend/src/lib/components/datatable/datatable-badge-colored.svelte
+++ frontend/src/lib/components/datatable/datatable-badge-colored.svelte
@@ -1,13 +1,13 @@
 <script lang="ts">
-  import Badge from "@/components/ui/badge/badge.svelte";
+    import Badge from "@/components/ui/badge/badge.svelte";
     import type { Component } from "@lucide/svelte";
 
-  export let color: string;
-  export let text: string;
-  export let icon: typeof Component
+    export let color: string;
+    export let text: string;
+    export let icon: typeof Component
 </script>
 
 <Badge variant="secondary" class="uppercase font-semibold gap-2">
-    <svelte:component this={icon} strokeWidth={3} class="{`text-${color}`}" />
+    <svelte:component this={icon} strokeWidth={3} class="{color}" />
     {text}
 </Badge>
